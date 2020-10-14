9e70dfa0551c085943ac4eb23e020ac48b323797
Emil_Wozniak <emil.wozniak.591986@gmail.com>
2020-10-14 10:37:32 +0200

    Updates

---
+++ precache-manifest.315143de5678c503d761aee8fbaa7a20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5f02115b4a5d065465b7ee8f9b1fee",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "ae5da397ebd7adfc0fc6",
+    "url": "/task/main.css"
+  },
+  {
+    "revision": "ae5da397ebd7adfc0fc6",
+    "url": "/task/static/css/main.3510c91e.chunk.css"
+  },
+  {
+    "revision": "a8e252651d8367fb0e53",
+    "url": "/task/static/js/2.37fa157d.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/task/static/js/2.37fa157d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5da397ebd7adfc0fc6",
+    "url": "/task/static/js/main.bb4cf8a5.chunk.js"
+  },
+  {
+    "revision": "d921765ce56f061e4525",
+    "url": "/task/static/js/runtime-main.3042ce32.js"
+  },
+  {
+    "revision": "0b9178965c6a4d114ba09e5138482aa7",
+    "url": "/task/static/media/me.0b917896.png"
+  }
+]);
